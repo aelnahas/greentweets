e542b260dc8bcf9cb526f72ee25d8fcf1cbea85a
Ahmed ElNahas <elnahas.ahmed@gmail.com>
2021-12-02 22:13:52 -0500

    setup heroku

--- routes.py
+++ routes.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+# from fastapi.middleware.cores import CORSMiddleware
 import queries
 
+origins = ["*"]
+
 app = FastAPI()
+
+# app.add_middleware(
+#   CORSMiddleware,
+#   allow_origins=origins,
+#   allow_credentials=True,
+#   allow_methods=["*"],
+#   allow_headers=["*"]
+# )
+
+@app.get("/")
+def get_root():
+  return {"Ping": "Pong"}
 
 @app.post("/queries/aggregates")
 def aggregations():
