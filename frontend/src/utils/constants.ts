88b3772c0ca1a79d5b5d2131b07e42c6158645cd
Ahmed ElNahas <elnahas.ahmed@gmail.com>
2021-12-03 04:40:35 -0500

    fix base url

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -3,5 +3,5 @@ export const BASE_URL = () => {
   if (process.env.NODE_ENV !== "production") {
     return "http://localhost:8000"
   }
-  return "https://arcane-woodland-78008.herokuapp.com/"
+  return "https://arcane-woodland-78008.herokuapp.com"
 }
